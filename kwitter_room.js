96390e8d4c9b39c57acb6a5035079a5298fb0c55
ripanwhitehatrj <88594549+ripanwhitehatrj@users.noreply.github.com>
2022-01-26 16:01:35 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -15,7 +15,14 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
     //Start code
-
+    console.log("roomname-"+Room_names);
+    row = "<div class = 'room_name' id = "+Room_names+" onclick = 'redirecttoroomname(this.id)'>#"+Room_names+"</div> <hr>";
+    document.getElementById("output").innerHTML += row;
     //End code
     });});}
 getData();
+      function redirecttoroomname(name) {
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location = "kwitter_page.html";
+}
